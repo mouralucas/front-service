4f6fbd0902b7faa206807955224b2fb1f9b03e12
Lucas Moura <lucaspenha471@gmail.com>
2025-04-09 09:15:29 -0300

    Remove margin for cards

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-# Frontend Services (React + Vite)
+# Frontend Service (React + Vite)
 
-## If error with scss occur 
+## Primeiros Passos
+
+## Instalação das dependências
+
+## Open source libs para gráficos
+
+[Recharts](https://recharts.org/en-US/)
+
+[Outros exemplos](https://www.reddit.com/r/reactjs/comments/1ddbqei/open_source_react_chart_libraries/)
+
+## Em caso de erros de compilação com o scss
 
 ```bash
 npm add -D sass-embedded
--- src/pages/DesignTests.tsx
+++ src/pages/DesignTests.tsx
@@ -1,61 +1,62 @@
-import {ReactElement, useCallback, useState} from "react";
-import DataGrid from '../components/table/DataGrid'
-import {Button as Btn} from "devextreme-react/data-grid";
-import Drawer from "../components/Drawer.tsx";
+import {ReactElement} from "react";
+import Card from "../components/Card.tsx";
 
 
 const App = (): ReactElement => {
-    const [isDrawerOpened, setIsDrawerOpened] = useState<boolean>(false);
-
-    const onOpenDrawerClick = useCallback(() => {
-        setIsDrawerOpened(!isDrawerOpened);
-    }, [isDrawerOpened]);
 
     return (
         <div className="container">
             <div className="row">
-                <div className="col-12">
-                    <DataGrid
-                        keyExpr={'id'}
-                        data={[
-                            {
-                                id: '1',
-                                name: 'Test 1',
-                            },
-                            {
-                                id: '2',
-                                name: 'Teste 2'
-                            }
-                        ]}
-                        columns={[
-                            {
-                                dataField: 'id',
-                                caption: 'ID',
-                                dataType: 'string'
-                            },
-                            {
-                                dataField: 'name',
-                                caption: 'Nome',
-                                dataType: 'string'
-                            },
-                            {
-                                caption: 'Ações',
-                                type: 'buttons',
-                                width: 110,
-                                child: [
-                                    <Btn
-                                        key={1}
-                                        text="Editar"
-                                        // icon="/url/to/my/icon.ico"
-                                        icon="edit"
-                                        hint="Abrir Drawer"
-                                        onClick={onOpenDrawerClick}
-                                    />
-                                ]
-                            }
-                        ]}
-                    />
-                    <Drawer isOpened={isDrawerOpened} changePanelOpened={onOpenDrawerClick} content={<></>}/>
+                <div className="col-3">
+                    <Card>
+                        <Card.Body>
+                            <>Card de descrição</>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="col-9">
+                    <div className="row">
+                        <div className="col-4">
+                            <Card>
+                                <Card.Body>
+                                    <p>Saldo</p>
+                                    <p>R$ 0,00</p>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="col-4">
+                            <Card>
+                                <Card.Body>
+                                    <p>Entradas</p>
+                                    <p>R$ 0,00</p>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="col-4">
+                            <Card>
+                                <Card.Body>
+                                    <p>Despesas</p>
+                                    <p>R$ 0,00</p>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-6">
+                            <Card>
+                                <Card.Body>
+                                    <>Lucas</>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div className="col-6">
+                            <Card>
+                                <Card.Body>
+                                    <>Lucas</>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
