f0263861bee7025ac4ee3ac93dc073d84eeeb7c0
Lucas Moura <lucaspenha471@gmail.com>
2025-06-05 00:18:28 -0300

    Add basic modal to create/edit brazilian funds (incomplete)

---
+++ src/pages/finance/administration/modals/BrazilianFunds.tsx
@@ -0,0 +1,53 @@
+import Modal from "../../../../components/Modal.tsx";
+import {BrazilianFunds} from "../../../../interfaces/Finance.tsx";
+import {Controller, useForm} from "react-hook-form";
+import {BaseSyntheticEvent} from "react";
+
+interface BrazilianFundsModalProps {
+    modalState: boolean;
+    hideModal: () => void;
+    brazilianFund?: BrazilianFunds;
+}
+
+
+const BrazilianFundsModal = (props: BrazilianFundsModalProps) => {
+    const {handleSubmit, control, formState: {errors}} = useForm<BrazilianFunds>();
+
+    const onSubmit = (data: BrazilianFunds, e: BaseSyntheticEvent<object> | undefined) => {
+        console.log(e, data);
+    }
+
+    const body =
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="row">
+                    <div className="col-6">
+                        <label htmlFor="">Nome do fundo</label>
+                        <Controller
+                            name={'name'}
+                            control={control}
+                            rules={{required: 'Esse campo é obrigatório'}}
+                            render={({field}) => (
+                                <input
+                                    type={'text'}
+                                    {...field}
+                                    className={`form-control input-default ${errors.name ? 'input-error' : ''}`}
+                                />
+                            )}
+                        />
+                    </div>
+                </div>
+            </form>
+        </>
+
+    return (
+        <Modal
+            title={'Fundos de Investimentos do Brasil'}
+            showModal={props.modalState}
+            hideModal={props.hideModal}
+            body={body}
+        />
+    )
+}
+
+export default BrazilianFundsModal;
--- src/pages/finance/administration/tables/BrazilianFunds.tsx
+++ src/pages/finance/administration/tables/BrazilianFunds.tsx
@@ -1,24 +1,41 @@
 import DataGrid from "../../../../components/table/DataGrid.tsx";
-import {DataGridColumn} from "../../../../assets/core/components/Interfaces.tsx";
+import {DataGridColumn, DataGridToolBarItem} from "../../../../assets/core/components/Interfaces.tsx";
 import {useEffect, useState} from "react";
 import {getBrazilianFunds} from "../../../../services/getCommonData/Finance.tsx";
 import Loader from "../../../../components/Loader.tsx";
+import BrazilianFundsModal from '../modals/BrazilianFunds.tsx'
+import {BrazilianFunds} from "../../../../interfaces/Finance.tsx";
+import Button from "devextreme-react/button";
 
 
 const BrazilianFundsTable = () => {
     const [isLoading, setIsLoading] = useState<boolean>(true)
+    const [brazilianFundsModalState, setBrazilianFundsModalState] = useState<boolean>(false)
 
-    const [brazilianFunds, setBrazilianFunds] = useState<Any>([])
+    const [brazilianFunds, setBrazilianFunds] = useState<any[]>([])
+    const [selectedBrazilianFund, setSelectedBrazilianFund] = useState<BrazilianFunds | undefined>()
 
     const fetchBrazilianFunds = async () => {
         setBrazilianFunds(await getBrazilianFunds(false));
         setIsLoading(false);
     }
 
+    const showBrazilianFundsModal = (e: any) => {
+        if (typeof e.row !== 'undefined') {
+            setSelectedBrazilianFund(e.row.data);
+        }
+
+        setBrazilianFundsModalState(true);
+    }
+
+    const hideBrazilianFundsModal = () => {
+        setSelectedBrazilianFund(undefined);
+        setBrazilianFundsModalState(false);
+    }
+
     useEffect(() => {
         fetchBrazilianFunds().then();
     }, []);
-
 
     const columns: DataGridColumn[] = [
         {
@@ -66,15 +83,46 @@ const BrazilianFundsTable = () => {
         },
     ]
 
+    const toolBarItems: DataGridToolBarItem[] = [
+        {
+            name: 'columnChooserButton',
+            location: 'after',
+        },
+        {
+            name: 'exportButton',
+            location: 'after',
+        },
+        {
+            child: <Button icon='refresh' onClick={fetchBrazilianFunds}/>,
+            location: "after"
+        },
+        {
+            child: <Button icon={'add'} onClick={showBrazilianFundsModal}></Button>,
+            location: "after"
+        },
+        {
+            name: 'searchPanel',
+            location: "after",
+        },
+    ]
+
     return (
         <>
             {isLoading ? <Loader/> :
                 <DataGrid
                     keyExpr={'fundId'}
                     data={brazilianFunds}
+                    toolBar={
+                        {
+                            visible: true,
+                            items: toolBarItems,
+                        }
+                    }
                     columns={columns}
                 />
+
             }
+            <BrazilianFundsModal modalState={brazilianFundsModalState} hideModal={hideBrazilianFundsModal} brazilianFund={selectedBrazilianFund}/>
         </>
     )
 }
