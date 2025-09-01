c2b4a3435e2a84cadee188eb5217503b5da7ecce
Lucas P. Moura <35279505+mouralucas@users.noreply.github.com>
2025-09-01 15:49:30 -0300

    Added skeleton to publisher table with mui

--- src/pages/library/backoffice/tables/Publishers.tsx
+++ src/pages/library/backoffice/tables/Publishers.tsx
@@ -1,122 +1,24 @@
-import DataGrid from "../../../../components/table/DataGrid.tsx";
-import {useEffect, useState} from "react";
-import {getPublishers} from "../../../../services/getCommonData/Library.tsx";
-import {toast} from "react-toastify";
-import {Button as Btn,} from 'devextreme-react/data-grid';
-import {DataGridColumn, DataGridToolBarItem} from "../../../../assets/core/components/Interfaces.tsx";
-import Loader from "../../../../components/Loader.tsx";
-import Button from "devextreme-react/button";
+import { ReactElement } from "react"
+import DataGridComp from "../../../../components/table/DataGridV2"
+import { Box, IconButton } from "@mui/material"
+import { GridColDef } from "@mui/x-data-grid";
+import { useQuery } from "@apollo/client"
+import { apolloLibraryClient } from '../../../../services/apollo/client/ApolloLibraryService';
 
-
-const App = () => {
-    const [publishers, setPublishers] = useState<any[]>([])
-
-    const [isLoading, setIsLoading] = useState<boolean>(true)
-
-    const getAvailablePublishers = async () => {
-        setIsLoading(true);
-        setPublishers(await getPublishers(false));
-
-        setIsLoading(false);
-    }
-
-    useEffect(() => {
-        getAvailablePublishers().then();
-    }, [])
-
-    const coffeeCommand = () => {
-        toast('☕ Cafezinho delícia!');
-    }
-
-    const columns: DataGridColumn[] = [
-        {
-            dataField: "publisherId",
-            caption: "Id",
-            dataType: "number",
-            width: 70,
-        },
-        {
-            dataField: "publisherName",
-            caption: "Nome",
-            dataType: "string",
-        },
-        {
-            dataField: "description",
-            caption: "Descrição",
-            dataType: "string",
-            format: 'shortDate',
-        },
-        {
-            dataField: "country_name",
-            caption: "Pais",
-            dataType: "string",
-        },
-        {
-            caption: 'Ações',
-            type: 'buttons',
-            width: 110,
-            child: [
-                // <Btn
-                //     key={1}
-                //     text="Editar"
-                //     // icon="/url/to/my/icon.ico"
-                //     icon="edit"
-                //     hint="Editar"
-                //     onClick={showModal}
-                // />,
-                <Btn
-                    key={2}
-                    //icon="/url/to/my/icon.ico"
-                    icon="coffee"
-                    hint="My Command"
-                    onClick={coffeeCommand}
-                />]
-        }
-    ]
-
-    const toolBarItems: DataGridToolBarItem[] = [
-        {
-            name: 'columnChooserButton',
-            location: 'after',
-        },
-        {
-            name: 'exportButton',
-            location: 'after',
-        },
-        {
-            child: <Button icon={'refresh'} onClick={getAvailablePublishers}/>,
-            location: "after"
-        },
-        // {
-        //     child: <Button icon={'add'} onClick={}></Button>,
-        //     location: "after"
-        // },
-        {
-            name: 'searchPanel',
-            location: "after",
-        },
-    ]
-
-    return (
-        <>
-            {isLoading ?
-                <Loader/>
-                :
-                <DataGrid
-                    keyExpr={'publisherId'}
-                    data={publishers}
-                    columns={columns}
-                    toolBar={
-                        {
-                            visible: true,
-                            items: toolBarItems
-                        }
-                    }
-                />
-            }
-            {/*<AuthorModal modalState={authorModalState} hideModal={hideAuthorModal} author={selectedAuthor}/>*/}
-        </>
+const PublisherTable = (): ReactElement => {
+    const { data: publishersDate, loading: pubishersLoading, refetch: publishersRefetch} = useQuery(QUERY_PUBLISHER, 
+        client: 
     )
+
+    const columns: GridColDef[] = [
+        { field: "publisherId", headerName: "Id"},
+        { field: "publisherName", headerName: "Nome", flex: 2 }, 
+        { field: "description", headerName: "Descrição", flex: 1 },
+        { field: "countryName", headerName: "País", flex: 1 }
+    ]
+
+
+    return (<></>)
 }
 
-export default App;
+export default PublisherTable;
