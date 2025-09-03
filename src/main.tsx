01810ce82dacd46eca83c4fca1d7fbd45691991b
Lucas Moura <lucaspenha471@gmail.com>
2025-09-03 09:46:10 -0300

    Removed unesed/old stylesheets from main

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import 'devextreme/dist/css/dx.light.css';
-import "react-datepicker/dist/react-datepicker.css";
+// import 'devextreme/dist/css/dx.light.css';
+// import "react-datepicker/dist/react-datepicker.css";
 import './index.css';
 import './assets/core/colors.css';
 import './assets/core/errors.css';
