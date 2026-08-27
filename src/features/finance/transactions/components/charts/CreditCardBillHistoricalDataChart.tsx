cc2e85851bdbe398b6ada61da9b0d1011d01f851
Lucas Moura <lucaspenha471@gmail.com>
2026-08-27 11:07:13 -0300

    Update datepicker from historical data chart

--- src/features/finance/transactions/components/charts/CreditCardBillHistoricalDataChart.tsx
+++ src/features/finance/transactions/components/charts/CreditCardBillHistoricalDataChart.tsx
@@ -1,31 +1,49 @@
 import { useQuery } from '@apollo/client';
-import { ptBR } from 'date-fns/locale';
-import { useEffect, useState } from "react";
-import DatePicker from "react-datepicker";
+import { useCallback, useEffect, useState } from "react";
 import BarChart from '../../../../../components/chart/BarChart.tsx';
+import { BarChartReferenceLine } from '../../../../../interfaces/BarChart.ts';
 import { apolloFinanceClient } from '../../../../../services/apollo/client/ApolloFinanceService.tsx';
 import { getLastPeriods, getPeriodFromDate } from "../../../../../utils/datetime.tsx";
 import { QUERY_CREDIT_CARDS_MONTHLY_BILL_EVOLUTION as QUERY_CREDIT_CARD_BILL_HITORICAL_DATA } from '../../api/queries.ts';
 import { GetCreditCardBillHistoricalDataQuery } from '../../types/CreditCardQueries.ts';
-import { BarChartReferenceLine } from '../../../../../interfaces/BarChart.ts';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { ptBR } from 'date-fns/locale';
 
 const CreditCardBillEvolution = () => {
-    const [dateRange, setDateRange] = useState<any[]>(getLastPeriods());
-    const [startDate, endDate] = dateRange;
+    // Filter date range
+    const [startDate, setStartDate] = useState<Date | null>(null);
+    const [endDate, setEndDate] = useState<Date | null>(null);
+
+    useEffect(() => {
+        const range = getLastPeriods();
+        setStartDate(new Date(range[0]));
+        setEndDate(new Date(range[1]));
+        updateDateRange(getLastPeriods());
+    }, []);
+
+
 
     const startPeriod = startDate ? getPeriodFromDate(startDate) : undefined;
     const endPeriod = endDate ? getPeriodFromDate(endDate) : undefined;
 
-    const { data: historyData } = useQuery<GetCreditCardBillHistoricalDataQuery>(QUERY_CREDIT_CARD_BILL_HITORICAL_DATA, {
+    const { data: historyData, refetch: historyRefetch } = useQuery<GetCreditCardBillHistoricalDataQuery>(QUERY_CREDIT_CARD_BILL_HITORICAL_DATA, {
         client: apolloFinanceClient,
         skip: startPeriod === undefined || endPeriod === undefined,
         variables: {
             params: {
-                startPeriod,
-                endPeriod
+                startPeriod: getPeriodFromDate(startDate),
+                endPeriod: getPeriodFromDate(endDate),
             }
         }
     })
+
+    const updateDateRange = useCallback((dates: any) => {
+        if (dates[1] !== null) {
+            historyRefetch();
+        }
+    }, [historyRefetch]);
 
     const historicalData = historyData?.getCreditCardBillHistoricalData.historicalData;
     const referenceLine: BarChartReferenceLine[] = historicalData
@@ -54,18 +72,44 @@ const CreditCardBillEvolution = () => {
                 <div className="col-4">
                 </div>
                 <div className="col-4">
-                    <DatePicker
-                        selectsRange={true}
-                        startDate={startDate}
-                        endDate={endDate}
-                        onChange={(update) => {
-                            setDateRange(update);
-                        }}
-                        showMonthYearPicker
-                        dateFormat={'MMM/yyyy'}
-                        locale={ptBR}
-                        className={'form-control'}
-                    />
+                    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBR}>
+                        <DatePicker
+                            label="Data inicial"
+                            views={["month", "year"]}
+                            value={startDate}
+                            onChange={(newValue) => {
+                                setStartDate(newValue);
+                                if (endDate && newValue && endDate < newValue) {
+                                    setEndDate(newValue);
+                                }
+                            }}
+                            maxDate={endDate || undefined}
+                            slotProps={{
+                                textField: {
+                                    size: "small",
+                                    fullWidth: false
+                                },
+                            }}
+                        />
+                        <DatePicker
+                            label="Data final"
+                            views={["month", "year"]}
+                            value={endDate}
+                            onChange={(newValue) => {
+                                setEndDate(newValue);
+                                if (startDate && newValue && startDate > newValue) {
+                                    setStartDate(newValue);
+                                }
+                            }}
+                            minDate={startDate || undefined}
+                            slotProps={{
+                                textField: {
+                                    size: "small",
+                                    fullWidth: false
+                                },
+                            }}
+                        />
+                    </LocalizationProvider>
                 </div>
             </div>
             <BarChart
